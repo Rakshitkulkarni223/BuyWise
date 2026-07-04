4247ba59a99e61278b187bebd9bae2308a43f6b9
rakshit.kulkarni <rakshit.kulkarni@servicenow.com>
2026-07-05 01:53:14 +0530

    backend remove unwanted files

--- backend/dist/routes/search.routes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const SearchController_1 = require("../controllers/SearchController");
-const auth_1 = require("../middleware/auth");
-const router = (0, express_1.Router)();
-router.post('/search', auth_1.authenticate, SearchController_1.SearchController.search);
-router.post('/recommendations', auth_1.authenticate, SearchController_1.SearchController.recommend);
-exports.default = router;
